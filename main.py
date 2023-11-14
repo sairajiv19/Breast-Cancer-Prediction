3f1c1d5a733f9df2ea067d0acc7aa08c260fb5af
S Sai Rajiv <142566227+sairajiv19@users.noreply.github.com>
2023-11-14 13:08:59 +0530

    Updated the main file
    Added a new patient record text file

--- main.py
+++ main.py
@@ -1,16 +1,111 @@
-from logistic_regression_model import logistic_accu
-from KNN_classification import knn_accu
-from linear_svm_classification import linear_svm_accu
-from kernel_svm_classification import rbf_svm_accu
-from naive_bayes_classification import naive_accu
-from decision_tree_classification import tree_accu
-from random_forest_classification import forest_accu
-
-if __name__ == '__main__':
-    print(f'Logistic Regression\'s Accuracy Score-> {logistic_accu}')
-    print(f'KNN Classification\'s Accuracy Score-> {knn_accu}')
-    print(f'Linear Kernel SVM Classification\'s Accuracy Score-> {linear_svm_accu}')
-    print(f'RBF Kernel SVM Classification\'s Accuracy Score-> {rbf_svm_accu}')
-    print(f'Gaussian Naive Bayes Classification\'s Accuracy Score-> {naive_accu}')
-    print(f'Decision Tree Classification\'s Accuracy Score-> {tree_accu}')
-    print(f'Random Forest Classification\'s Accuracy Score-> {forest_accu}')
+import colorama
+import re
+import numpy as np
+from colorama import Fore
+import logistic_regression_model as lr
+import KNN_classification as knn
+import linear_svm_classification as l_svm
+import kernel_svm_classification as rbf_svm
+import naive_bayes_classification as naive
+import decision_tree_classification as dtc
+import random_forest_classification as rfc
+colorama.init(autoreset=True)
+classifier = rfc.classifier
+print("------------------Breast Cancer Prediction------------------")
+while True:
+    print("1-Make a prediction\n"
+          "2-Retrieve patient data\n"
+          "3-Change prediction model\n"
+          "4-Exit")
+    option = input('Please select an action:')
+    if option == '1':
+        sample_number = int(input("Enter Sample Number:"))
+        print("Enter the following perimeters to make the prediction")
+        x_0 = int(input('Clump thickness:'))
+        x_1 = int(input('Uniformity of Cell Size:'))
+        x_2 = int(input('Uniformity of Cell Shape:'))
+        x_3 = int(input('Marginal Adhesion:'))
+        x_4 = int(input('Single Epithelial Cell Size:'))
+        x_5 = int(input('Bare Nuclei:'))
+        x_6 = int(input("Bland Chromatin:"))
+        x_7 = int(input("Normal Nucleoli:"))
+        x_8 = int(input("Mitoses:"))
+        X = np.array([[x_0, x_1, x_2, x_3, x_4, x_5, x_6, x_7, x_8]])
+        prediction = classifier.predict(X)
+        print("--------------------Result--------------------")
+        if prediction == 2:
+            prediction = 'Benign Cell'
+            print(Fore.GREEN + prediction)
+        elif prediction == 4:
+            prediction = 'Malignant Cell'
+            print(Fore.RED + prediction)
+        print("--------------------Result--------------------")
+        f = open("Patient Record.txt", mode='a')
+        f.writelines(
+            [f"Patient Number: {sample_number}\n", f"Clump thickness: {x_0}\n", f"Uniformity of Cell Size: {x_1}\n",
+             f"Uniformity of Cell Shape: {x_2}\n", f"Marginal Adhesion: {x_3}\n", f"Single Epithelial Cell Size: {x_4}\n",
+             f"Bare Nuclei: {x_5}\n", f"Bland Chromatin: {x_6}\n", f"Normal Nucleoli: {x_7}\n", f"Mitoses: {x_8}\n",
+             f"Result: {prediction}\n", "------------------------------------------------------------\n"
+             ])
+        print(Fore.GREEN + "***************Patient-Record-Updated***************")
+        f.close()
+    elif option == '2':
+        f = open("Patient Record.txt", 'r')
+        num = input('Patient Number:')
+        n = 0
+        while True:
+            counter = 0
+            f.seek(n + 16)
+            data = f.read(6)
+            f.seek(n)
+            record = f.read(232)
+            r = re.findall(r'10', record[22::])
+            if len(r) != 0:
+                record += f.read(len(r))
+            if data == '':
+                print(colorama.Fore.RED + 'Invalid Patient Number!')
+                break
+            if record[-11] == 'B':
+                if data == num:
+                    print('************************************************************************')
+                    print(colorama.Fore.GREEN + record)
+                    print('************************************************************************')
+                    break
+                else:
+                    f.seek(0)
+                    n = n + 306 + len(r)
+            elif record[-11] == 'M':
+                if data == num:
+                    record = record + f.read(3)
+                    print('************************************************************************')
+                    print(colorama.Fore.RED + record)
+                    print('************************************************************************')
+                    break
+                else:
+                    f.seek(0)
+                    n = n + 309 + len(r)
+        f.close()
+    elif option == '3':
+        print("Below you can see the various model\'s score")
+        print(f'1.Logistic Regression\'s Accuracy Score-> {lr.logistic_accu}')
+        print(f'2.KNN Classification\'s Accuracy Score-> {knn.knn_accu}')
+        print(f'3.Linear Kernel SVM Classification\'s Accuracy Score-> {l_svm.linear_svm_accu}')
+        print(f'4.RBF Kernel SVM Classification\'s Accuracy Score-> {rbf_svm.rbf_svm_accu}')
+        print(f'5.Gaussian Naive Bayes Classification\'s Accuracy Score-> {naive.naive_accu}')
+        print(f'6.Decision Tree Classification\'s Accuracy Score-> {dtc.tree_accu}')
+        print(f'7.Random Forest Classification\'s Accuracy Score-> {rfc.forest_accu}')
+        model = int(input("Select the model number:"))
+        model_list = [lr.classifier, knn.classifier, l_svm.classifier, rbf_svm.classifier, naive.classifier,
+                      dtc.classifier, rfc.classifier]
+        name_list = ["Logistic Regression", "KNN Classification", "Linear Kernel SVM Classification",
+                     "RBF Kernel SVM Classification", "Gaussian Naive Bayes Classification",
+                     "Decision Tree Classification", "Random Forest Classification"]
+        classifier = model_list[model-1]
+        print("************************************************************************")
+        print(Fore.GREEN + f"Successfully changed the prediction model to {name_list[model-1]}")
+        print("************************************************************************")
+    elif option == '4':
+        exit()
+    else:
+        print('Invalid Option!')
+        exit()
